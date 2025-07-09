2abecb8c118a10eee52dbdd4cb7907994ed25559
anupriya0520 <147934661+anupriya0520@users.noreply.github.com>
2025-07-09 19:25:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int n=72;
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum+=i;
+        }
+        System.out.println(sum/n);
+    }
+}
