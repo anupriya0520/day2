e73042c97f9d5d634da155702ed588c4fbbb8efa
anupriya0520 <147934661+anupriya0520@users.noreply.github.com>
2025-07-09 13:48:39 +0530

    Add files via upload

---
+++ defaultvalues.java
@@ -0,0 +1,21 @@
+class defaultvalues {
+       byte b;
+       short s;
+       int i;
+       long l;
+       float f;
+       double d;
+       char ch;
+       boolean isboolean;
+    public static void main(String[] args) {
+        defaultvalues m1=new defaultvalues();
+       System.out.println(m1.b);
+       System.out.println(m1.s);
+       System.out.println(m1.i);
+       System.out.println(m1.l);
+       System.out.println(m1.f);
+       System.out.println(m1.d);
+       System.out.println(m1.ch);
+       System.out.println(m1.isboolean);
+    }
+}
